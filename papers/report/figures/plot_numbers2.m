c38beaf34d2b7dd3faa9403362703e6210b32440
lg-zhang <zhanglg921@gmail.com>
2016-01-12 02:55:18 -0500

    final version

--- papers/report/figures/plot_numbers2.m
+++ papers/report/figures/plot_numbers2.m
@@ -5,15 +5,17 @@ load('data2.mat');
 figure('units','normalized','position',[.1 .1 .4 .5])
 
 plot(1:5, ...
-        strong_read(1, (1:5)+offset), 'o-', 'lineWidth', 2, 'MarkerSize',7)
-
+        mix_read(1, (1:5)), 'o-', 'lineWidth', 2, 'MarkerSize',7)
+hold on
+plot(1:5, ...
+        mix_write(1, (1:5)), 'o-', 'lineWidth', 2, 'MarkerSize',7)
+    
 set(gca,'fontsize',14)
-xlabel('percentage of write operations')
+xlabel('percentage of eventually consistent operations operations')
 ylabel('average latency(ms)');
 axis([1 5 0 inf]);
-legend({'strongly consistent read', 'strongly consistent write', ...
-        'eventually consistent read', 'eventually consistent write'}, 'fontsize', 12);
+legend({'read', 'write'}, 'fontsize', 12);
 ax = gca;
 ax.XTick = [1 2 3 4 5];
 ax.XTickLabel = {'0%', '25%', '50%', '75%', '100%'};
-title('0ms latency added');
+title('Effect of mixing different consistency models');
